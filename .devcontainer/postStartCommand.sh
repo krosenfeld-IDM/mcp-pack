cd2b37ca922379ef537d7eb5c3dedcd13c1c2695
MeWu-IDM <40474438+MeWu-IDM@users.noreply.github.com>
2025-05-01 18:01:30 -0700

    fix some issue

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 6333:6333 -p 6334:6334 -v $(pwd)/qdrant_db:/qdrant/storage:z qdrant/qdrant
+docker run -d -p 6333:6333 -p 6334:6334 -v $CODESPACE_VSCODE_FOLDER/qdrant_db:/qdrant/storage:z qdrant/qdrant
+export UV_HTTP_TIMEOUT=120
 cd src/mcp_langchain_agent
 uv run python -m streamlit run app.py
