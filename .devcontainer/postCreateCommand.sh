f46698deb70e8002c3c1ef1486e5131216d2a97e
MeWu-IDM <40474438+MeWu-IDM@users.noreply.github.com>
2025-05-01 18:26:05 -0700

    try postStart for docker ops

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-docker run -d -p 6333:6333 -p 6334:6334 -v "$CODESPACE_VSCODE_FOLDER/qdrant_db:/qdrant/storage:z" qdrant/qdrant
 pip install uv
 pip install -e .
-mcp_pack create_db https://github.com/starsimhub/starsim --verbose --include-notebooks --include-rst
-mcp_pack create_db https://github.com/sciris/sciris --verbose --include-notebooks --include-rst
-mcp_pack list_db
-    
--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 docker run -d -p 6333:6333 -p 6334:6334 -v $CODESPACE_VSCODE_FOLDER/qdrant_db:/qdrant/storage:z qdrant/qdrant
+sleep 30
 export UV_HTTP_TIMEOUT=120
+
+mcp_pack create_db https://github.com/starsimhub/starsim --verbose --include-notebooks --include-rst
+mcp_pack create_db https://github.com/sciris/sciris --verbose --include-notebooks --include-rst
+mcp_pack list_db
+    
+	
 cd src/mcp_langchain_agent
 uv run python -m streamlit run app.py
