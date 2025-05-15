da3c1fea2dadd266e8da62cf6a10f4c93e613925
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-05-15 21:12:09 +0000

    Bump version: 0.1.2 → 0.2.0

--- src/mcp_pack/version.py
+++ src/mcp_pack/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ["__version__", "__versiondate__", "__license__"]
 
-__version__ = "0.1.2"
-__versiondate__ = '2025-04-29'
+__version__ = "0.2.0"
+__versiondate__ = '2025-05-15'
 __license__ = "Apache-2.0"
